ffa9c32543016df55ce3058abc83bd01f30a3c3a
Vadim Gorbachev <vadim.gorbachev.main@gmail.com>
2019-11-25 03:42:53 +0300

    added samples

--- Uchat Tut App/Messenger/MessengerViewController.swift
+++ Uchat Tut App/Messenger/MessengerViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class MessengerViewController: UITableViewController {
     
     private let cellId = "chatsList"
+    let friendNames = ["Алиса","Бобби", "Боб", "Роб", "Роберт", "Боберт"]
+    let lastSentenses = ["Каеф", "Завтра утром посмотрю", "Окич", "Фига. Название интригующее", "Ахахах, орево", "Это тру"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +28,15 @@ class MessengerViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return 6
+        return friendNames.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: CustomMessengerCell.reuseId, for: indexPath) as! CustomMessengerCell
-        cell.friendIcon.backgroundColor = .red
+        cell.friendIcon.backgroundColor = UIColor(red: 0.775, green: 0, blue: 0, alpha: 1)
         cell.friendIcon.layer.cornerRadius = cell.friendIcon.frame.height / 2
-        
+        cell.friendName.text = friendNames[indexPath.row]
+        cell.lastMessageText.text = lastSentenses[indexPath.row]
         return cell
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
